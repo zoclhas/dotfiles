b98abc6dd56815e6ea3de28a8a56df903d9725e4
Zoclhas <zoclhas@gmail.com>
2024-03-20 00:08:33 +0400

    made autotag actually function

--- nvim/lua/custom/plugins/coding.lua
+++ nvim/lua/custom/plugins/coding.lua
@@ -196,6 +196,11 @@ return {
             node_decremental = 'V',
           },
         },
+        -- Auto tag
+        autotag = {
+          enable = true,
+          filetypes = { 'html', 'javascriptreact', 'typescriptreact', 'javascript' },
+        },
       }
     end,
   },
@@ -347,30 +352,42 @@ return {
     end,
   },
 
+  -- {
+  --   'NvChad/nvim-colorizer.lua',
+  --   opts = function()
+  --     require('colorizer').setup {
+  --       filetypes = { '*' },
+  --       user_default_options = {
+  --         RGB = true, -- #RGB hex codes
+  --         RRGGBB = true, -- #RRGGBB hex codes
+  --         names = true, -- "Name" codes like Blue or blue
+  --         RRGGBBAA = true, -- #RRGGBBAA hex codes
+  --         AARRGGBB = true, -- 0xAARRGGBB hex codes
+  --         rgb_fn = true, -- CSS rgb() and rgba() functions
+  --         hsl_fn = true, -- CSS hsl() and hsla() functions
+  --         css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+  --         css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+  --         -- Available modes for `mode`: foreground, background,  virtualtext
+  --         mode = 'background', -- Set the display mode.
+  --         -- Available methods are false / true / "normal" / "lsp" / "both"
+  --         -- True is same as normal
+  --         tailwind = true, -- Enable tailwind colors
+  --         -- parsers can contain values used in |user_default_options|
+  --         sass = { enable = true, parsers = { 'css' } }, -- Enable sass colors
+  --         virtualtext = '■',
+  --       },
+  --     }
+  --   end,
+  -- },
+
   {
-    'norcalli/nvim-colorizer.lua',
-    opts = function()
-      require('colorizer').setup {
-        filetypes = { '*' },
-        user_default_options = {
-          RGB = true, -- #RGB hex codes
-          RRGGBB = true, -- #RRGGBB hex codes
-          names = true, -- "Name" codes like Blue or blue
-          RRGGBBAA = true, -- #RRGGBBAA hex codes
-          AARRGGBB = true, -- 0xAARRGGBB hex codes
-          rgb_fn = true, -- CSS rgb() and rgba() functions
-          hsl_fn = true, -- CSS hsl() and hsla() functions
-          css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-          css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
-          -- Available modes for `mode`: foreground, background,  virtualtext
-          mode = 'background', -- Set the display mode.
-          -- Available methods are false / true / "normal" / "lsp" / "both"
-          -- True is same as normal
-          tailwind = true, -- Enable tailwind colors
-          -- parsers can contain values used in |user_default_options|
-          sass = { enable = true, parsers = { 'css' } }, -- Enable sass colors
-          virtualtext = '■',
-        },
+    'zoclhas/nvim-highlight-colors',
+    config = function()
+      require('nvim-highlight-colors').setup {
+        render = 'background',
+        enable_named_colors = true,
+        enable_tailwind = true,
+        enable_document_color = true,
       }
     end,
   },
