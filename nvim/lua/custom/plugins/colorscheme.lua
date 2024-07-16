111720508b2e61671175cc2d5308aa86fb1c12e6
zoclhas <zoclhas@gmail.com>
2024-07-16 14:01:18 +0800

    Start new config

---
+++ nvim/lua/custom/plugins/colorscheme.lua
@@ -0,0 +1,92 @@
+return {
+  'rebelot/kanagawa.nvim',
+  lazy = false,
+  priority = 1000,
+  config = function()
+    local function ternary(cond, T, F)
+      if cond then
+        return T
+      else
+        return F
+      end
+    end
+
+    require('kanagawa').setup {
+      compile = true,
+      undercurls = true,
+
+      transparent = ternary(vim.g.neovide, false, true),
+      terminalColors = true,
+
+      colors = {
+        theme = {
+          all = {
+            ui = {
+              bg_gutter = 'none',
+            },
+          },
+        },
+      },
+
+      overrides = function(colors)
+        local theme = colors.theme
+        return {
+          NormalFloat = { bg = 'none' },
+          FloatBorder = { bg = 'none' },
+          FloatTitle = { bg = 'none' },
+
+          NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+          LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+          MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+
+          Heading1 = { fg = colors.palette.peachRed, bold = true },
+          Heading2 = { fg = colors.palette.waveRed, bold = true },
+          Heading3 = { fg = colors.palette.sakuraPink, bold = true },
+          Heading4 = { fg = colors.palette.carpYellow, bold = true },
+          HeadingRest = { fg = colors.palette.boatYellow2, bold = true },
+
+          Bold = { fg = colors.palette.boatYellow2, bold = true },
+          Italics = { fg = colors.palette.sakuraPink, italic = true },
+          Quotes = { fg = colors.palette.dragonBlue, italic = true },
+
+          ['@string.regexp'] = { link = '@string.regex' },
+          ['@variable.parameter'] = { link = '@parameter' },
+          ['@exception'] = { link = '@exception' },
+          ['@string.special.symbol'] = { link = '@symbol' },
+          ['@markup.strong'] = { link = '@text.strong', bold = true },
+          ['@markup.italic'] = { link = '@text.emphasis', italic = true },
+          ['@markup.underline'] = { link = '@text.underline' },
+          ['@markup.heading'] = { link = 'Function' },
+          ['@markup.raw'] = { link = '@text.literal' },
+          ['@markup.quote'] = { link = '@text.quote' },
+          ['@markup.math'] = { link = 'Constant' },
+          ['@markup.environment'] = { link = '@text.environment' },
+          ['@markup.environment.name'] = { link = '@text.environment.name' },
+          ['@markup.link.url'] = { link = 'Special' },
+          ['@markup.link.label'] = { link = 'Identifier' },
+          ['@comment.note'] = { link = '@text.note' },
+          ['@comment.warning'] = { link = '@text.warning' },
+          ['@comment.danger'] = { link = '@text.danger' },
+          ['@diff.plus'] = { link = '@text.diff.add' },
+          ['@diff.minus'] = { link = '@text.diff.delete' },
+          ['@markup.link.url.markdown_inline'] = { link = 'Special' }, -- (url)
+          ['@markup.link.label.markdown_inline'] = { link = 'WarningMsg' }, -- [label]
+          ['@markup.italic.markdown_inline'] = { link = 'Italics' }, -- *italic*
+          ['@markup.strong.markdown_inline'] = { link = 'Bold' }, -- **strong**
+          ['@markup.raw.markdown_inline'] = { link = 'String' }, -- `code`
+          ['@markup.list.markdown'] = { link = 'Function' }, -- + list
+          ['@markup.quote.markdown'] = { link = 'Quotes' }, -- > blockcode
+          ['@markup.heading.1.markdown'] = { link = 'Heading1' },
+          ['@markup.heading.2.markdown'] = { link = 'Heading2' },
+          ['@markup.heading.3.markdown'] = { link = 'Heading3' },
+          ['@markup.heading.4.markdown'] = { link = 'Heading4' },
+          ['@markup.heading.5.markdown'] = { link = 'HeadingRest' },
+          ['@markup.heading.6.markdown'] = { link = 'HeadingRest' },
+        }
+      end,
+    }
+
+    vim.cmd.colorscheme 'kanagawa-dragon'
+    vim.cmd.hi 'Comment gui=none'
+  end,
+}
